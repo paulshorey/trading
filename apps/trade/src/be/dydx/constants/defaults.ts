e1d2931bd32fa188aa02234f969b9dd8a14cdba0
Paul Shorey <hello@paulshorey.com>
2024-11-12 13:11:08 -0600

    adjust sl

--- apps/trade/src/be/dydx/constants/defaults.ts
+++ apps/trade/src/be/dydx/constants/defaults.ts
@@ -1,15 +1,15 @@
 export const defaults = {
   default: {
-    LONG: 1.5,
-    SHORT: 1.5,
+    LONG: 5,
+    SHORT: 5,
     precision: 1,
   },
   'SOL-USD': {
     precision: 0.1,
   },
   'SUI-USD': {
-    LONG: 2,
-    SHORT: 2,
+    LONG: 7.5,
+    SHORT: 7.5,
     precision: 10,
   },
   'ETH-USD': {
@@ -19,8 +19,6 @@ export const defaults = {
     precision: 0.01,
   },
   'BTC-USD': {
-    LONG: 1,
-    SHORT: 1,
     precision: 0.0001,
   },
   'SUNDOG-USD': {

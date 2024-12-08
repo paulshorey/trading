9186e19afe2f2a2ec1f4bac06f37f7b3a32a404c
Paul Shorey <hello@paulshorey.com>
2024-12-08 01:09:13 -0600

    upgraded codebase, monorepo, auth

--- my/fe/src/auth/hooks/useSessionOrLoginClient.ts
+++ my/fe/src/auth/hooks/useSessionOrLoginClient.ts
@@ -1,20 +1,20 @@
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
-import * as React from 'react';
-import { useEffect } from 'react';
-import { SessionContext } from '@src/context/SessionProvider';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation'
+import * as React from 'react'
+import { useEffect } from 'react'
+import { SessionContext } from '../../../src/auth/context/SessionProvider'
 
 export default function useSessionOrLogin() {
-  const session = React.useContext(SessionContext);
-  const router = useRouter();
-  const path = usePathname();
-  const searchParams = useSearchParams();
-  const url = `${path}${searchParams.toString() ? '?' : ''}${searchParams.toString()}`;
+  const session = React.useContext(SessionContext)
+  const router = useRouter()
+  const path = usePathname()
+  const searchParams = useSearchParams()
+  const url = `${path}${searchParams.toString() ? '?' : ''}${searchParams.toString()}`
 
   useEffect(() => {
     if (!session?.user?.auth) {
-      router.push(`/auth?redirect=${encodeURIComponent(url)}`);
+      router.push(`/auth?redirect=${encodeURIComponent(url)}`)
     }
-  }, []);
+  }, [])
 
-  return session;
+  return session
 }

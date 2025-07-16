5ddd4e9d2ce3fb721ee3fea81e40e548860e61d5
Paul Shorey <hello@paulshorey.com>
2025-07-16 16:50:01 -0500

    added polling and timer to ui

--- apps/trade/src/app/my/page.tsx
+++ apps/trade/src/app/my/page.tsx
@@ -1,34 +1,5 @@
-import { Data } from '@src/fe/blocks/Data'
-import { infoAccount } from '@src/be/dydx/infoAccount'
+import { PageAccount } from '@src/fe/pages/PageAccount'
 
-export const revalidate = 0
-
-export default async function Page({}: {}) {
-  const data = await infoAccount()
-  if (!data) {
-    throw new Error('No data from DYDX')
-  }
-  if (data.orders) {
-    let orders = {} as Record<string, any>
-    for (let row of data.orders) {
-      orders[`${row.ticker} ${row.size} ${row.status}`] = row
-    }
-    data.orders = orders
-  }
-  if (data.asksAndBids) {
-    let asks = {} as Record<string, any>
-    let bids = {} as Record<string, any>
-    if (data.asksAndBids?.asks.length) {
-      data.asksAndBids?.asks.forEach((row: any, i: number) => {
-        asks[row.price.toString()] = row.size
-      })
-    }
-    if (data.asksAndBids?.asks.length) {
-      data.asksAndBids?.bids.forEach((row: any, i: number) => {
-        bids[row.price.toString()] = row.size
-      })
-    }
-    data.asksAndBids = { asks, bids }
-  }
-  return <Data data={data} expandUntil={5} />
+export default function Page() {
+  return <PageAccount />
 }
--- apps/trade/src/fe/blocks/Data.tsx
+++ apps/trade/src/fe/blocks/Data.tsx
@@ -3,9 +3,6 @@
 import classes from './Data.module.scss'
 import { Json } from '@my/fe/src/components/blocks/Json'
 import { Collapsed } from '@my/fe/src/components/blocks/Collapsed'
-// import LocalShortTime from '@my/fe/src/components/inline/LocalShortTime'
-// import Badge from '@my/fe/src/components/inline/Badge'
-// import Link from 'next/link'
 
 export function Data({ data, expandUntil }: { data: Record<string, unknown>; expandUntil: number }) {
   const sections = Object.entries(data).map(([key, obj], i: number) => {
@@ -20,35 +17,6 @@ export function Data({ data, expandUntil }: { data: Record<string, unknown>; exp
         }}
         key={i}
         title={heading}
-        // buttonsRight={[
-        //   <Badge className="font-bold pr-2">
-        //     <Link key="edit" href={`/?name=${log.name}`}>
-        //       {' '}
-        //       {log.name}{' '}
-        //     </Link>
-        //   </Badge>,
-        //   <Badge className="font-bold pr-2">
-        //     <Link key="edit" href={`/?app_name=${log.app_name}`}>
-        //       {' '}
-        //       {log.app_name}{' '}
-        //     </Link>
-        //   </Badge>,
-        //   <Badge className="font-bold pr-2">
-        //     <Link key="edit" href={`/?server_name=${log.server_name}`}>
-        //       {' '}
-        //       {log.server_name}{' '}
-        //     </Link>
-        //   </Badge>,
-        //   <Badge className="font-bold pr-2">
-        //     <Link key="edit" href={`/?dev=${log.dev}`}>
-        //       {' '}
-        //       {log.dev ? 'dev' : 'pro'}{' '}
-        //     </Link>
-        //   </Badge>,
-        //   <Badge key="time">
-        //     <LocalShortTime epoch={log.time} />
-        //   </Badge>,
-        // ]}
         openDefault={i === 0}
         isClickToToggle
         className="relative px-4 pt-3 pb-3 border-b border-gray-600 "

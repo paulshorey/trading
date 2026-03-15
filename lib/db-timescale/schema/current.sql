1c318f8e3cb58f18c921f5b4ff5063777cbea42e
Paul Shorey <hello@paulshorey.com>
2026-03-14 23:51:42 -0500

    Cursor: Apply local changes for cloud agent

--- lib/db-timescale/schema/current.sql
+++ lib/db-timescale/schema/current.sql
@@ -2,6 +2,7 @@
 -- PostgreSQL database dump
 --
 
+
 -- Dumped from database version 17.7
 -- Dumped by pg_dump version 18.2
 
@@ -17,9 +18,37 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+--
+
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: bbo-1s; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public."bbo-1s" (
+    "time" timestamp with time zone NOT NULL,
+    ticker text NOT NULL,
+    open numeric,
+    high numeric,
+    low numeric,
+    close numeric,
+    volume numeric,
+    cvd numeric
+);
+
 
 --
 -- Name: candles-1m; Type: TABLE; Schema: public; Owner: -
@@ -51,18 +80,37 @@ CREATE TABLE public."candles-1m" (
 
 
 --
--- Name: bbo-1s; Type: TABLE; Schema: public; Owner: -
+-- Name: candles_1h_1m; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public."bbo-1s" (
+CREATE TABLE public.candles_1h_1m (
     "time" timestamp with time zone NOT NULL,
     ticker text NOT NULL,
-    open numeric,
-    high numeric,
-    low numeric,
-    close numeric,
-    volume numeric,
-    cvd numeric
+    symbol text,
+    open double precision NOT NULL,
+    high double precision NOT NULL,
+    low double precision NOT NULL,
+    close double precision NOT NULL,
+    volume double precision DEFAULT 0 NOT NULL,
+    ask_volume double precision DEFAULT 0 NOT NULL,
+    bid_volume double precision DEFAULT 0 NOT NULL,
+    cvd_open double precision,
+    cvd_high double precision,
+    cvd_low double precision,
+    cvd_close double precision,
+    vd double precision,
+    vd_ratio double precision,
+    book_imbalance double precision,
+    price_pct double precision,
+    divergence double precision,
+    trades integer DEFAULT 0 NOT NULL,
+    max_trade_size double precision DEFAULT 0 NOT NULL,
+    big_trades integer DEFAULT 0 NOT NULL,
+    big_volume double precision DEFAULT 0 NOT NULL,
+    sum_bid_depth double precision DEFAULT 0 NOT NULL,
+    sum_ask_depth double precision DEFAULT 0 NOT NULL,
+    sum_price_volume double precision DEFAULT 0 NOT NULL,
+    unknown_volume double precision DEFAULT 0 NOT NULL
 );
 
 
@@ -94,41 +142,6 @@ CREATE TABLE public.candles_1m_1s (
     book_imbalance double precision,
     price_pct double precision,
     divergence double precision,
-    sum_bid_depth double precision DEFAULT 0 NOT NULL,
-    sum_ask_depth double precision DEFAULT 0 NOT NULL,
-    sum_price_volume double precision DEFAULT 0 NOT NULL,
-    unknown_volume double precision DEFAULT 0 NOT NULL
-);
-
-
---
--- Name: candles_1h_1m; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.candles_1h_1m (
-    "time" timestamp with time zone NOT NULL,
-    ticker text NOT NULL,
-    symbol text,
-    open double precision NOT NULL,
-    high double precision NOT NULL,
-    low double precision NOT NULL,
-    close double precision NOT NULL,
-    volume double precision DEFAULT 0 NOT NULL,
-    ask_volume double precision DEFAULT 0 NOT NULL,
-    bid_volume double precision DEFAULT 0 NOT NULL,
-    cvd_open double precision,
-    cvd_high double precision,
-    cvd_low double precision,
-    cvd_close double precision,
-    vd double precision,
-    vd_ratio double precision,
-    book_imbalance double precision,
-    price_pct double precision,
-    divergence double precision,
-    trades integer DEFAULT 0 NOT NULL,
-    max_trade_size double precision DEFAULT 0 NOT NULL,
-    big_trades integer DEFAULT 0 NOT NULL,
-    big_volume double precision DEFAULT 0 NOT NULL,
     sum_bid_depth double precision DEFAULT 0 NOT NULL,
     sum_ask_depth double precision DEFAULT 0 NOT NULL,
     sum_price_volume double precision DEFAULT 0 NOT NULL,
@@ -197,14 +210,6 @@ ALTER TABLE ONLY public.candles_1s
 
 
 --
--- Name: candles_1m_1s candles_1m_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.candles_1m_1s
-    ADD CONSTRAINT candles_1m_pkey PRIMARY KEY (ticker, "time");
-
-
---
 -- Name: candles_1h_1m candles_1h_1m_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -213,24 +218,18 @@ ALTER TABLE ONLY public.candles_1h_1m
 
 
 --
+-- Name: candles_1m_1s candles_1m_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.candles_1m_1s
+    ADD CONSTRAINT candles_1m_pkey PRIMARY KEY (ticker, "time");
+
+
+--
 -- Name: candles-1m_time_idx; Type: INDEX; Schema: public; Owner: -
 --
 
 CREATE INDEX "candles-1m_time_idx" ON public."candles-1m" USING btree ("time" DESC);
-
-
---
--- Name: idx_candles_1m_1s_time_desc; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_candles_1m_1s_time_desc ON public.candles_1m_1s USING btree ("time" DESC);
-
-
---
--- Name: idx_candles_1h_1m_time_desc; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_candles_1h_1m_time_desc ON public.candles_1h_1m USING btree ("time" DESC);
 
 
 --
@@ -252,6 +251,20 @@ CREATE INDEX idx_bbo_1s_ticker ON public."bbo-1s" USING btree (ticker);
 --
 
 CREATE INDEX idx_bbo_1s_time ON public."bbo-1s" USING btree ("time");
+
+
+--
+-- Name: idx_candles_1h_1m_time_desc; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX idx_candles_1h_1m_time_desc ON public.candles_1h_1m USING btree ("time" DESC);
+
+
+--
+-- Name: idx_candles_1m_1s_time_desc; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX idx_candles_1m_1s_time_desc ON public.candles_1m_1s USING btree ("time" DESC);
 
 
 --
@@ -287,4 +300,10 @@ CREATE INDEX idx_hour ON public."candles-1m" USING btree (EXTRACT(hour FROM ("ti
 --
 
 CREATE INDEX idx_minute ON public."candles-1m" USING btree (EXTRACT(minute FROM ("time" AT TIME ZONE 'UTC'::text)));
+
+
+--
+-- PostgreSQL database dump complete
+--
+
 
